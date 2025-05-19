024cb4aaad71cb74cb2a845357150cbed78ca321
Surya Prakash <surya.jinagam@gmail.com>
2025-05-19 19:05:04 +0530

    Api responses

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+// here we try to specify the limit of the json type data that our server can accept
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// here we are trying to take the data from the url
+
+app.use(express.static("public"))
+// if there are any assets like file or images we keep them in the public folder
+
+app.use(cookieParser())
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path:'./env '
 })
+
+
+app.on("error",(error)=>{
+    console.error("Database connection FAILED",error)
+    process.exit(1)
+})
+
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`The server is listening at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MONGODB connection FAILED: ",err);
+})
 
 
 
